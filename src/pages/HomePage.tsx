b1f7f0e60090d4e73574fec092d0094ca2e2f6a0
Grubenko Maxim <mdgrubenko@maieducation>
2023-12-10 04:28:27 +0300

    add adaptive styles in HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,15 @@
+import styles from './HomePage.module.css'
+
 const HomePage = () => (
-    <div className="lg:flex flex-row bg-red-700 justify-between">
-        {/* Первая колонка */}
-        <div className="h-full w-[70%] lg:border-l bg-red-300">
+    <div className={styles['homepage-container']}>
+        <div className={styles['booking-card-container']}>
             <div className="h-full px-4 py-6 lg:px-8 flex flex-col">
                 <p className="mb-4">vwerbiwnevinwiebnins wei iwineivb</p>
                 <div className="mb-4"><a href="#">vbneibieribei</a></div>
-                <button className="bg-blue-500 text-white px-4 py-2">Моя Кнопка</button>
+                {/*<button className="bg-blue-500 text-white px-4 py-2">Моя Кнопка</button>*/}
             </div>
         </div>
-        {/* Вторая колонка */}
-        <div className="w-[30%] lg:border-l bg-red-400">
+        <div className={styles['homepage-right-menu']}>
             <div className="h-full px-4 py-6 lg:px-8 flex flex-col">
                 <p className="mb-4">vwerbiwnevinwiebnins wei iwineivb</p>
                 <div className="mb-4"><a href="#">vbneibieribei</a></div>
