34a27330f1a827053198b949be1b97edaca3fc9d
Grubenko Maxim <mdgrubenko@maieducation>
2023-12-10 03:57:38 +0300

    add adaptive styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import {BrowserRouter as Router, Redirect, Route, RouteProps, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, RouteProps, Switch } from 'react-router-dom';
 import '@/styles/global.css';
 import AdminPage from "@/pages/AdminPage.tsx";
 import HomePage from "@/pages/HomePage.tsx";
 import LoginPage from "@/pages/LoginPage.tsx";
-import {Sidebar} from "@/components/Sidebar/Sidebar.tsx";
-import React, {ReactNode} from "react";
+import { Sidebar } from "@/components/Sidebar/Sidebar.tsx";
+import React, { ReactNode } from "react";
+import './App.css';
 
 const SidebarNavUnits = [
-    {text: 'Главная', path: '/main', JSXContent: <HomePage/>},
-    {text: 'Бронирование аудиторий', path: '/booking', JSXContent: <div>Бронирование аудиторий</div>},
-    {text: 'Инвентаризация', path: '/inventory', JSXContent: <div>Инвентаризация</div>},
-    {text: 'Администрирование', path: '/admin', JSXContent: <AdminPage/>},
-    {text: 'Расписание', path: '/schedule', JSXContent: <div>Расписание</div>},
+    { text: 'Главная', path: '/main', JSXContent: <HomePage /> },
+    { text: 'Бронирование аудиторий', path: '/booking', JSXContent: <div>Бронирование аудиторий</div> },
+    { text: 'Инвентаризация', path: '/inventory', JSXContent: <div>Инвентаризация</div> },
+    { text: 'Администрирование', path: '/admin', JSXContent: <AdminPage /> },
+    { text: 'Расписание', path: '/schedule', JSXContent: <div>Расписание</div> },
 ];
 
 interface PrivateRouteProps extends RouteProps {
     jsxContent: ReactNode;
 }
 
-const PrivateRoute: React.FC<PrivateRouteProps> = ({jsxContent, ...rest}) => {
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ jsxContent, ...rest }) => {
     const user = "useAuth()";
 
     return (
@@ -26,13 +27,18 @@ const PrivateRoute: React.FC<PrivateRouteProps> = ({jsxContent, ...rest}) => {
             {...rest}
             render={() =>
                 user ? (
-                    <div className="col-span-5 lg:col-span-3">
-                        <div className="h-full px-4 py-6 lg:px-8">
-                            {jsxContent}
+                    <div className="app-container">
+                        <div className="sidebar-container">
+                            <Sidebar navUnits={SidebarNavUnits} />
+                        </div>
+                        <div className="content-container">
+                            <div className="h-full px-4 py-6 lg:px-8">
+                                {jsxContent}
+                            </div>
                         </div>
                     </div>
                 ) : (
-                    <Redirect to="/login"/>
+                    <Redirect to="/login" />
                 )
             }
         />
@@ -43,36 +49,26 @@ function App() {
     return (
         <Router>
             <div className="bg-background text-foreground">
-                {/*<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl lg:border-b">*/}
-                {/*    Smart Campus*/}
-                {/*</h1>*/}
                 <Switch>
                     {/* Страница логина */}
                     <Route path="/login">
-                        <LoginPage/>
+                        <LoginPage />
                     </Route>
                     {/* Остальные страницы с Sidebar */}
                     <Route path="/">
-                        <div className="grid lg:grid-cols-6">
-                            <div className="col-span-1 lg:col-span-1">
-                                <Sidebar navUnits={SidebarNavUnits}/>
-                            </div>
-                            <div className='col-span-5 lg:col-span-5'>
-                                <Switch>
-                                    {SidebarNavUnits.map((navUnit, index) => (
-                                        <PrivateRoute
-                                            key={index}
-                                            path={navUnit.path}
-                                            jsxContent={navUnit.JSXContent}
-                                        />
-                                    ))}
-                                    {/* Дополнительный маршрут для отлавливания несуществующих путей */}
-                                    <Route path="*">
-                                        <Redirect to="/main"/>
-                                    </Route>
-                                </Switch>
-                            </div>
-                        </div>
+                        <Switch>
+                            {SidebarNavUnits.map((navUnit, index) => (
+                                <PrivateRoute
+                                    key={index}
+                                    path={navUnit.path}
+                                    jsxContent={navUnit.JSXContent}
+                                />
+                            ))}
+                            {/* Дополнительный маршрут для отлавливания несуществующих путей */}
+                            <Route path="*">
+                                <Redirect to="/main" />
+                            </Route>
+                        </Switch>
                     </Route>
                 </Switch>
             </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,23 @@
 const HomePage = () => (
-    <div className='grid lg:grid-cols-7'>
-        <div className="col-span-5 lg:col-span-5 lg:border-l">
-            <div className="h-full px-4 py-6 lg:px-8">
-                <p>vwerbiwnevinwiebnins wei iwineivb</p>
-                <div><a>vbneibieribei</a></div>
-                <button>Моя Кнопка</button>
+    <div className="lg:flex flex-row bg-red-700 justify-between">
+        {/* Первая колонка */}
+        <div className="h-full w-[70%] lg:border-l bg-red-300">
+            <div className="h-full px-4 py-6 lg:px-8 flex flex-col">
+                <p className="mb-4">vwerbiwnevinwiebnins wei iwineivb</p>
+                <div className="mb-4"><a href="#">vbneibieribei</a></div>
+                <button className="bg-blue-500 text-white px-4 py-2">Моя Кнопка</button>
             </div>
         </div>
-        <div className="col-span-2 lg:col-span-2 lg:border-l">
-            <div className="h-full px-4 py-6 lg:px-8">
-                <p>vwerbiwnevinwiebnins wei iwineivb</p>
-                <div><a>vbneibieribei</a></div>
-                <button>Моя Кнопка</button>
+        {/* Вторая колонка */}
+        <div className="w-[30%] lg:border-l bg-red-400">
+            <div className="h-full px-4 py-6 lg:px-8 flex flex-col">
+                <p className="mb-4">vwerbiwnevinwiebnins wei iwineivb</p>
+                <div className="mb-4"><a href="#">vbneibieribei</a></div>
+                <button className="bg-blue-500 text-white px-4 py-2">Моя Кнопка</button>
             </div>
         </div>
     </div>
+
 );
 
 export default HomePage;
