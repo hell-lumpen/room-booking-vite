f960bf6638521f6b5b2d6fd0c564f58e0c9bf59e
Grubenko Maxim <mdgrubenko@maieducation>
2024-01-10 19:16:53 +0300

    -

--- src/components/BrandHeader.tsx
+++ src/components/BrandHeader.tsx
@@ -14,14 +14,13 @@ const BrandHeader = () => {
             className="flex items-center max-w-screen p-2 pb-0 md:max-w-full cursor-pointer"
             onClick={handleClick}
         >
-            <div className="w-auto h-full mr-2">
-                <BrandLogo color={"hsl(var(--primary))"} imageHeight={60} className={'max-w-full h-auto'}/>
+            <div className="hidden lg:block w-auto h-full mr-2">
+                <BrandLogo color={"hsl(var(--primary))"} imageHeight={60} className='max-w-full h-auto'/>
             </div>
             <div className="flex flex-col ml-2">
-                <h1 className="text-4xl font-extrabold tracking-tight text-primary">
+                <h1 className="text-2xl lg:text-3xl font-extrabold tracking-tight text-primary">
                     Smart Campus
                 </h1>
-                {/*<p className="text-secondary-foreground text-sm">Оптимизация рабочих процессов</p>*/}
             </div>
         </div>
     );
